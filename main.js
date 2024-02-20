173dc6b8a58f2260bb1d67ba099db6eea3aeefb7
Seungho Shin <nsmusic80@naver.com>
2024-02-21 01:00:13 +0900

    api change

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const API_Key=`e0fce2d4211a448fb98af0c7169b9658`
 let news=[];
 const getLatestNews = async()=>{
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_Key}`);
-    const url = new URL(`https://noona-news-api-test.netlify.app?q=음악&page=1&pageSize=15&category=entertainment`);
+    const url = new URL(`https://noona-news-api-test.netlify.app/top-headlines?page=1&pagesize=15&category=entertainment`);
     const response = await fetch(url);
+    console.log("rrr", response);
     const data = await response.json();
     news = data.articles;
     console.log("ddd", news);
