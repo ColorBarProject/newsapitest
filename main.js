85a7ccbf71a2a85d5b0c5f25b837a660ab8d565c
Seungho Shin <nsmusic80@naver.com>
2024-02-25 02:10:08 +0900

    page nav

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 // 보완 예정 2: 검색결과 0건이면 "결과 없음" 출력
 // 보완 예정 3: 기사 날짜표시 년/월/일로 표시
 
-// const API_Key = `e0fce2d4211a448fb98af0c7169b9658`;
-
+const API_Key = `e0fce2d4211a448fb98af0c7169b9658`;
 const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("search-input");
 const searchEntChk = document.querySelector("#search-input");
 const menus = document.querySelectorAll(".menus button");
 const sideMenu = document.querySelectorAll(".side-menu-list button");
 const titleClk = document.querySelector(".title-logo");
-let imageUrl = "";
-let newsHTML = "";
+
 let newsList = [];
 let qKey = ""; // search-input값을 넣을 전역변수
 let categoryKey = ""; //카테고리 값을 넣을 전역변수
-// let pageNum = "";               //보여줄 페이지 번호
-// let pageSizeKey = "20";          //페이지당 보여줄 결과물 갯수
+let page = 1;               //지금 페이지 번호
+let pageSizeKey = 20;          //페이지당 보여줄 결과물 갯수
+let totalResults = 0;
+let pageGroupSize = 5;
 
 menus.forEach((menu) =>
   menu.addEventListener("click", (event) => {
@@ -80,17 +80,19 @@ const closeNav = () => {
   document.getElementById("mySidenav").style.width = "0";
 };
 
-const getNewsByCategory = async (event) => {
+const getNewsByCategory = async () => {
   try {
     const url = new URL(
-      `https://noona-news-api-test.netlify.app/top-headlines?q=${qKey}&category=${categoryKey}`
+      `https://noona-news-api-test.netlify.app/top-headlines?q=${qKey}&category=${categoryKey}&page=${page}&pagesize=${pageSizeKey}`
     );
     const response = await fetch(url);
     console.log("rrr by cate", response);
     const data = await response.json();
     if (response.status === 200) {
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      pagiNationRender ();
     } else {
       throw new Error(data.message);
     }
@@ -99,32 +101,24 @@ const getNewsByCategory = async (event) => {
   }
 };
 
-const validateImageUrl = (imgUrl) => {
-  const image = new Image();
-  image.src = imgUrl;
-  console.log("imgurl", imgUrl);
-  console.log(image.complete);
-  return image.complete || image.width + image.height > 0;
-};
+const imgError = (image) => {
+  image.onerror = null;
+  image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU";
+}; 
 
 const render = () => {
   qKey !== ""
     ? searchAlarm()
     : (document.getElementById("search-result").innerHTML = "");
   console.log(newsList.length);
-  newsHTML =
+  const newsHTML =
     newsList.length === 0
       ? `<div class="alert alert-danger" role="alert"> 검색 결과가 없습니다 ! </div>`
       : newsList
-          .map((news) => {
-            const validatedImage = validateImageUrl(news.urlToImage);
-            console.log(news.urlToImage);
-            console.log(validatedImage);
+          .map((news) => {            
             return `<div class="row news">
     <div class="col-lg-4">
-      <img class="news-img-size" src="${
-        validatedImage ? news.urlToImage : "imgs/no-image.png"
-      }"/>
+      <img class="news-img-size" src="${news.urlToImage}" alt="뉴스 이미지" onerror="imgError(this)"/>
     </div>    
     <div class="col-lg-8">
       <h2>${
@@ -139,11 +133,7 @@ const render = () => {
           ? news.description.substring(0, 100) + "..." //200자 너무 길어서 100자만... 제목은 40자만...
           : news.description
       }</p>      
-      <div>${news.source.name} * ${news.publishedAt} - ${moment(
-              news.publishedAt
-            )
-              .startOf("day")
-              .fromNow()}</div>
+      <div>${news.source.name || "출처 불명"} * ${news.publishedAt} - ${moment(news.publishedAt).fromNow()}</div>
     </div>
   </div>`;
           })
@@ -155,14 +145,44 @@ const render = () => {
 getNewsByCategory();
 
 function searchAlarm() {
-  document.getElementById(
-    "search-result"
-  ).innerHTML = `<p>검색어 "${qKey}"로 "${
-    categoryKey == "" ? "전체" : categoryKey
-  }"카테고리에서 검색한 결과입니다</p>`;
+  document.getElementById("search-result").innerHTML = `<p>검색어 "${qKey}"로 "${categoryKey == "" ? "전체" : categoryKey}"카테고리에서 검색한 결과입니다</p>`;
+};
+
+const pagiNationRender= ()=>{  
+  const pageGroup = Math.ceil(page / pageGroupSize);
+  const totalPages = Math.ceil(totalResults/pageGroupSize);
+  let lastPage = pageGroup * pageGroupSize;
+  if (lastPage > totalPages) {lastPage=totalPages};
+  const firstPage = lastPage - (pageGroupSize - 1) <= 0 ? 1 : lastPage - (pageGroupSize - 1) ;
+  let pagiNationHTML = `<li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">Previous</a></li>`;
+  for (let i=firstPage;i<=lastPage;i++) {
+    pagiNationHTML+=`<li class="page-item ${i===page?"active":""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+  }
+  pagiNationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">Next</a></li>`;
+  document.querySelector(".pagination").innerHTML = pagiNationHTML;
 }
 
+const moveToPage=(pageNum)=>{
+  page=pageNum;
+  getNewsByCategory()
+
+}
 const errorRender = (errorMessage) => {
   const errorHTML = `<div class="alert alert-danger" role="alert"> ${errorMessage} </div>`;
   document.getElementById("news-board").innerHTML = errorHTML;
 };
+
+
+// <li class="page-item">
+//             <a class="page-link" href="#" aria-label="Previous">
+//               <span aria-hidden="true">&laquo;</span>
+//             </a>
+//           </li>
+//           <li class="page-item"><a class="page-link" href="#">1</a></li>
+//           <li class="page-item"><a class="page-link" href="#">2</a></li>
+//           <li class="page-item"><a class="page-link" href="#">3</a></li>
+//           <li class="page-item">
+//             <a class="page-link" href="#" aria-label="Next">
+//               <span aria-hidden="true">&raquo;</span>
+//             </a>
+//           </li>
